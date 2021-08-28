88cfb119246b6003e1ff21954e68cac11cf3c860
maris <marisandb@yahoo.com>
2021-08-27 22:07:26 -0500

    added ability to vote

--- models/index.js
+++ models/index.js
@@ -1,4 +1,28 @@
 const User = require("./User");
 const Post = require("./Post");
+const Vote = require('./Vote');
 
-module.exports = { User, Post };
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+  User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+  });
+  
+  Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+  });
+
+
+module.exports = { User, Post, Vote };
